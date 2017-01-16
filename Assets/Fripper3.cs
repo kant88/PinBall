a34dcf1a076104993f44c896cd5769c065185181
aki <kom88aki@yahoo.co.jp>
2017-01-16 23:08:40 +0900

    発展課題までやりました。よろしくお願いします。

---
+++ Assets/Fripper3.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fripper3 : MonoBehaviour {
+	//HingiJointコンポーネントを入れる
+	private HingeJoint myHingeJoynt;
+
+	//初期の傾き
+	private float defaultAngle = 20;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		//HinjiJointコンポーネント取得
+		this.myHingeJoynt = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		bool flicked = false; // フリックされたかどうか
+
+		for (int i = 0; i < Input.touchCount; i++) {
+			if (Input.touches [i].position.x < Screen.width / 2 && tag == "LeftFripperTag") {
+				SetAngle (this.flickAngle);
+				flicked = true;
+			}
+			if (Input.touches [i].position.x >= Screen.width / 2 && tag == "RightFripperTag") {
+				SetAngle (this.flickAngle);
+				flicked = true;
+			}
+		}
+
+		if( flicked == false ) {
+			SetAngle (this.defaultAngle);
+		}
+	}
+
+	//フリッパーの傾きを設定
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoynt.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoynt.spring = jointSpr;
+	}
+}
---
+++ Assets/Scoring.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour {
+	private GameObject scoreText;
+	int point = 0;
+	// Use this for initialization
+	void Start () {
+		this.scoreText = GameObject.Find ("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.scoreText.GetComponent<Text> ().text = this.point.ToString() + "point";
+	}
+	 
+	void OnCollisionEnter(Collision other){
+		if (other.transform.tag == "SmallStarTag") {
+			this.point += 10;
+		} else if (other.transform.tag == "LargeStarTag") {
+			this.point += 50;
+		} else if (other.transform.tag == "SmallCloudTag") {
+			this.point += 20;
+		} else if (other.transform.tag == "LargeCloudTag") {
+			this.point += 100;
+		}
+	}
+}
